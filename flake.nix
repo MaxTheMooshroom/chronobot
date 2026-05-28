7c744a497cc18bf5c4bf2dd6595fe8abc3ac15ad
Maxine Zick <maxine@pnk.dev>
2026-05-28 19:18:13 -0400

    annotate nixpkgs input for patching reasons

--- flake.nix
+++ flake.nix
@@ -2,6 +2,14 @@
   description = "Discord bot for running chronomutants.";
 
   inputs = {
+    # Using branch `nixos-25.11` instead of tag `25.11` because
+    # rust builds in the current (as of writing) nixpkgs release
+    # are broken by the crates.io UA changes. nixos-* branches
+    # still receive maintainance after release, unlike tagged
+    # releases.
+    #
+    # For more information, see:
+    # https://github.com/rust-lang/crates.io/issues/13482
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -46,11 +54,8 @@
           {
             devShells.default = pkgs.mkShell {
               packages = with pkgs; [
-                # rustup
                 cargo
-                cargo-cache
                 cargo-workspaces
-                # rustc
                 clippy
                 rustfmt
               ];
