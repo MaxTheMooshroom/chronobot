f05fa53692e2e2769271ee7b5528236463b7305a
Maxine Zick <25956545+MaxTheMooshroom@users.noreply.github.com>
2026-06-12 21:15:27 -0400

    partial code restructure as a `Runtime` object gets built for managing async contexts ; added flux to the flake and as a dependency to the cargo workspace and core crate

---
+++ crates/core/src/runtime/mod.rs
@@ -0,0 +1,31 @@
+pub(crate) mod log;
+mod signal;
+
+use anyhow::Result;
+use tokio::runtime::Runtime;
+
+use std::cell::OnceCell;
+use std::future::Future;
+
+pub struct AsyncExecutor {
+    rt: OnceCell<Runtime>,
+}
+
+impl AsyncExecutor {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn block_on<F: Future>(&self, future: F) -> F::Output {
+        self.rt.get().unwrap().block_on(future)
+    }
+
+    pub fn run_main<F: Future<Output = !>>(self, _main: fn() -> F) -> ! {
+        todo!()
+    }
+
+    fn spawn(&mut self) -> Result<()> {
+        todo!()
+    }
+}
+
